c08ef08b8977164ed293cf118265611f59ed32df
deltafixer <gospodarweba@gmail.com>
2020-06-03 16:27:46 +0200

    feat(storybooks): add storybook config

---
+++ .storybook/main.js
@@ -0,0 +1,17 @@
+const path = require('path');
+
+module.exports = {
+  stories: ['../src/components/**/*.stories.mdx'],
+  addons: [
+    '@storybook/preset-create-react-app',
+    '@storybook/addon-knobs/register',
+    '@storybook/addon-actions/register',
+    '@storybook/addon-backgrounds/register',
+    {
+      name: '@storybook/addon-docs',
+      options: {
+        configureJSX: true,
+      },
+    },
+  ],
+};
