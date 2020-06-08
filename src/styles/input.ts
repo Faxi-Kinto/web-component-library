46eab95bb01d646f49e5c46bcdbf63bbcdffa4f3
Jonjoe Whitfield <me@jonjoe.io>
2020-06-08 22:36:16 +0100

    (fix): component errors

--- src/styles/input.ts
+++ src/styles/input.ts
@@ -1,9 +1,7 @@
 import { css } from 'styled-components';
-import { theme } from '../config';
 import { fontSize } from './basics';
 
 export const label = css`
-  color: ${theme.pallet.GRAY};
   display: inline-flex;
   height: 24px;
   align-items: flex-end;
