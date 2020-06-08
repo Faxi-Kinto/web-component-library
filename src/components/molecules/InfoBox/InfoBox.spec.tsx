b7a1d56f2db76ae276f5f59d6cc17d815ce438d6
Jonjoe Whitfield <me@jonjoe.io>
2020-06-08 22:33:58 +0100

    (fix): component errors

--- src/components/molecules/InfoBox/InfoBox.spec.tsx
+++ src/components/molecules/InfoBox/InfoBox.spec.tsx
@@ -4,7 +4,7 @@ import InfoBox from './InfoBox.component';
 
 test('InfoBox renders correctly', () => {
   const component = renderer.create(
-    <InfoBox icon={'question-circle'}>
+    <InfoBox>
       <p>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
         tempor incididunt ut labore et dolore magna aliqua.
