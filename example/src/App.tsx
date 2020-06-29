0e175394f2fe8a13557269e95dc2ab9ae6052d6c
deltafixer <gospodarweba@gmail.com>
2020-06-29 15:48:22 +0200

    chore(example): update example project

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,14 +1,21 @@
 import React from 'react';
-import { Button, Input, Icon } from 'probica13';
+import { Button, Input } from 'web-component-library';
+import Icon from 'fontAwesomeConfig';
+import 'index.css';
 
 const App = () => {
   return (
-    <div>
-      <Input />
-      <Button />
-      <Icon name="caps-lock" size={'14px'} />
-
-      <div>asdsdasd</div>
+    <div style={{ width: '500px' }}>
+      <Input label="Input field" placeholder="You could type" />
+      <Button
+        background="darkred"
+        fontColor="white"
+        width="300px"
+        height="50px"
+      >
+        You could click
+      </Button>
+      <Icon name="eye" size={'14px'} />
     </div>
   );
 };
---
+++ example/src/fontAwesomeConfig.ts
@@ -0,0 +1,30 @@
+import {
+  setLibraries,
+  setMapNamePropToFaNames,
+  Icon,
+  IconProps,
+} from 'web-component-library';
+import { IconProp } from '@fortawesome/fontawesome-svg-core';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
+
+setLibraries([fab, far]);
+
+export type IName = 'twitter' | 'eye';
+
+const customMapNameToFa = (iconName: IName): IconProp => {
+  switch (iconName) {
+    case 'twitter':
+      return ['fab', 'twitter'];
+
+    case 'eye':
+      return ['far', 'eye'];
+
+    default:
+      return ['far', 'eye'];
+  }
+};
+
+setMapNamePropToFaNames(customMapNameToFa);
+
+export default Icon as React.FC<IconProps<IName>>;
