7697d5a92c3b4d80b3d46f9229c9b562284ca8b0
dejanajan <dxy.jxy@gmail.com>
2020-07-10 11:16:27 +0200

    code optimisation and fixes

--- src/components/_molecules/CookiePolicy/CookiePolicy.spec.tsx
+++ src/components/_molecules/CookiePolicy/CookiePolicy.spec.tsx
@@ -7,8 +7,8 @@ test('CookiePolicy renders correctly', () => {
     <CookiePolicy
       title={'Cookie Policy'}
       text={`Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s.`}
-      buttonContentF={<p>Accept</p>}
-      buttonContentS={<p>Reject</p>}
+      acceptButtonContent={<p>Accept</p>}
+      declineButtonContent={<p>Reject</p>}
     />
   );
 
--- src/components/_molecules/CookiePolicy/CookiePolicy.styles.tsx
+++ src/components/_molecules/CookiePolicy/CookiePolicy.styles.tsx
@@ -1,28 +1,33 @@
 import styled from 'styled-components';
 import { pxToRem, padding } from '@faxi/web-css-utilities';
 
+type CookiePolicyStylingProps = {
+  bgColor?: string;
+  textColor?: string;
+  buttonColor?: string;
+  buttonBackground?: string;
+};
+
 export const Container = styled.div`
   .cookie-policy {
     ${padding(pxToRem('20px'), pxToRem('50px'))};
-    background-color: #00708d;
+    background-color: ${(props: CookiePolicyStylingProps) =>
+      `${props.bgColor}`};
 
     &__title,
     &__text {
       display: block;
       margin-bottom: ${pxToRem('20px')};
-      color: #fff;
+      color: ${(props: CookiePolicyStylingProps) => `${props.textColor}`};
     }
 
-    &__btnF,
-    &__btnS {
-      display: inline-flex;
-      justify-content: space-between;
-      flex-direction: row;
-      align-items: center;
+    &__accept-button,
+    &__decline-button {
       ${padding(pxToRem('10px'), pxToRem('35px'))};
       margin-right: ${pxToRem('20px')};
-      background-color: #fff;
-      color: #00708d;
+      background-color: ${(props: CookiePolicyStylingProps) =>
+        `${props.buttonBackground}`};
+      color: ${(props: CookiePolicyStylingProps) => `${props.buttonColor}`};
     }
   }
 `;
