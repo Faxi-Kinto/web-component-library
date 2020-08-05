b41376baf5c0c2d3c1030762ffdf4ec2fd0f9814
dejanajan <dxy.jxy@gmail.com>
2020-08-05 17:16:09 +0200

    fixes for sync code style and rename some fields

--- src/components/_molecules/Expander/Expander.spec.tsx
+++ src/components/_molecules/Expander/Expander.spec.tsx
@@ -5,12 +5,17 @@ import Expander from './Expander.component';
 test('Expander renders correctly', () => {
   const component = renderer.create(
     <Expander
-      label={'Explanation'}
-      text={`This graph shows the number of users who have successfully joined your community. 
+      title={'Explanation'}
+      body={`This graph shows the number of users who have successfully joined your community. 
   The higher the number, the easier it is for people to find someone else to share their journeys with. 
   Increase it by inviting more people or talk to your account manager about how you can promote your community.`}
-      // eslint-disable-next-line jsx-a11y/alt-text
-      icon={<img style={{ width: '14px' }} src="assets/icons/angle-down.svg" />}
+      icon={
+        <img
+          style={{ width: '14px' }}
+          src="assets/icons/angle-down.svg"
+          alt=""
+        />
+      }
     />
   );
 
