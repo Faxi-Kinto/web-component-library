2042ba90f1fdf47e40b1b569752a1507ef529d11
deltafixer <gospodarweba@gmail.com>
2020-10-05 15:23:41 +0200

    fix(TextArea): inherit font family

--- src/components/_atoms/TextArea/TextArea.styles.tsx
+++ src/components/_atoms/TextArea/TextArea.styles.tsx
@@ -4,13 +4,13 @@ import { marginChildren, pxToRem, padding } from '@faxi/web-css-utilities';
 export const TextAreaStyled = styled.div`
   width: 100%;
   ${marginChildren(`0 0 ${pxToRem('10px')} 0`)};
-  font-family: inherit;
 
   > textarea {
     width: 100%;
     height: ${pxToRem('45px')};
     ${padding(pxToRem('8px'), pxToRem('16px'))};
     outline: none;
+    font-family: inherit;
 
     &:focus,
     &.textarea--error {
