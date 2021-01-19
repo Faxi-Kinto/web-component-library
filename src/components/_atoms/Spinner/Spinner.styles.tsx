b8ef975814a677f23999382fb1c193f0703d0002
Marko Tomasevic <markotomasevic@MacBook-Pro.local>
2021-01-19 23:43:05 +0100

    feat(Spinner): add new spinner component

--- src/components/_atoms/Spinner/Spinner.styles.tsx
+++ src/components/_atoms/Spinner/Spinner.styles.tsx
@@ -1,32 +1,56 @@
 import styled from 'styled-components';
-import { size } from '@faxi/web-css-utilities';
 import { pxToRem } from '@faxi/web-css-utilities';
 
-type SpinnerStylingProps = {
-  color: string;
-  backgroundColor?: string;
-};
-
 export const Spinner = styled.div`
-  margin: auto;
-  ${size(pxToRem('36px'))};
-  border: ${(props: SpinnerStylingProps) =>
-    `${pxToRem('4px')} ${props.backgroundColor} solid`};
-  border-left: ${(props: SpinnerStylingProps) =>
-    `${pxToRem('4px')} ${
-      props.backgroundColor ? props.color : 'transparent'
-    } solid`};
-  border-radius: 50%;
-  animation: spin 1s infinite linear;
+  &.spinner {
+    ${(props: { size: number; color: string; backgroundColor: string }) => {
+      return `
+      width: ${pxToRem(`${props.size}px`)};
+      height: ${pxToRem(`${props.size}px`)};
+      background-color: ${props.backgroundColor};
+      `;
+    }};
+  }
 
-  @keyframes spin {
-    from {
-      transform: rotate(0deg);
-      transform: rotate(0deg);
+  * {
+    box-sizing: border-box;
+  }
+
+  .spinner-part {
+    overflow: hidden;
+    height: ${props => pxToRem(`${props.size / 2}px`)};
+    width: ${props => pxToRem(`${props.size}px`)};
+  }
+  .spinner-part.bottom {
+    transform: rotate(180deg) scale(-1, 1);
+  }
+  .spinner-rotator {
+    width: ${props => pxToRem(`${props.size}px`)};
+    height: ${props => pxToRem(`${props.size}px`)};
+    border: ${props => pxToRem(`${props.size / 7}px`)} solid transparent;
+    border-right-color: ${props => props.color};
+    border-top-color: ${props => props.color};
+    border-radius: 50%;
+    box-sizing: border-box;
+    animation: spinner-animation 3000ms ease-in-out infinite;
+    transform: rotate(-200deg);
+  }
+  @keyframes spinner-animation {
+    0% {
+      border-width: ${props => pxToRem(`${props.size / 7}px`)};
     }
-    to {
-      transform: rotate(359deg);
-      transform: rotate(359deg);
+    25% {
+      border-width: ${props => pxToRem(`${props.size / 23.33}px`)};
+    }
+    50% {
+      transform: rotate(115deg);
+      border-width: ${props => pxToRem(`${props.size / 7}px`)};
+    }
+    75% {
+      border-width: ${props => pxToRem(`${props.size / 23.33}px`)};
+    }
+    100% {
+      border-width: ${props => pxToRem(`${props.size / 7}px`)};
     }
   }
 `;
