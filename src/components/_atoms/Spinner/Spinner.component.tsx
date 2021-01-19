bf7393f8ac9c80b0e0791f1b308ae58926337757
Marko Tomasevic <markotomasevic@MacBook-Pro.local>
2021-01-19 23:43:05 +0100

    feat(Spinner): add new spinner component

--- src/components/_atoms/Spinner/Spinner.component.tsx
+++ src/components/_atoms/Spinner/Spinner.component.tsx
@@ -11,23 +11,28 @@ import * as Styled from './Spinner.styles';
 
 export type SpinnerProps = {
   color: string;
+  size: number;
   backgroundColor?: string;
   className?: string;
 };
 
 const Spinner: React.FC<SpinnerProps> = (props: SpinnerProps): JSX.Element => {
-  const { color, backgroundColor = 'transparent', className } = props;
+  const { color, size, backgroundColor = 'transparent', className } = props;
   return (
     <Styled.Spinner
       color={color}
-      backgroundColor={backgroundColor}
-      className={className}
-    />
+      size={size}
+      backgroundColor={backgroundColor && backgroundColor}
+      className={`spinner${className ? ' ' + className : ''}`}
+    >
+      <div className="spinner-part top">
+        <div className="spinner-rotator" />
+      </div>
+      <div className="spinner-part bottom">
+        <div className="spinner-rotator" />
+      </div>
+    </Styled.Spinner>
   );
-};
-
-Spinner.defaultProps = {
-  backgroundColor: 'transparent',
 };
 
 export default Spinner;
