6d9f419beafb3a9dc1cb6db48ffeb2fe819521ac
deltafixer <gospodarweba@gmail.com>
2021-01-20 14:04:49 +0100

    refactor(Button): make Button more generic

--- src/components/_atoms/Button/Button.styles.tsx
+++ src/components/_atoms/Button/Button.styles.tsx
@@ -1,21 +1,10 @@
 import styled from 'styled-components';
-import {
-  size,
-  pxToRem,
-  flexRow,
-  marginChildren,
-  padding,
-} from '@faxi/web-css-utilities';
 
 export const Button = styled.button`
-  ${padding(pxToRem('15px'), pxToRem('50px'))};
   border: none;
   cursor: pointer;
   font: inherit;
-
-  p {
-    margin: 0;
-  }
+  padding: unset;
 
   &:focus {
     outline: none;
@@ -24,15 +13,6 @@ export const Button = styled.button`
   &.button {
     &--secondary {
       background: transparent;
-      font-size: ${pxToRem('14px')};
-      ${flexRow('center', 'center')};
-      ${size('fit-content')};
-      padding: 0;
-      ${marginChildren(`0 ${pxToRem('12px')} 0 0`)}
-
-      p {
-        text-decoration: underline;
-      }
     }
 
     &--disabled {
